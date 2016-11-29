667aab52ad688003d469bd2a96b03f020e4dbcdc
Patrick LUZOLO <eldorplus@gmail.com>
2016-11-29 03:08:03 +0100

    jQuery adding

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,24 @@ elixir.config.js.browserify.transformers.push({
 });
 
 /**
+ * elixir.config.js.browserify.watchify.enabled = true;
+ * if(elixir.isWatching()){
+ *  elixir.config.js.browserify.plugins.push({
+ *      name: "browserify-hmr",
+ *      options : {}
+ *   });
+ * }
+ *
+ * require('laravel-elixir-vueify');
+ * elixir.config.js.browserify.plugins.push({
+ *   name: 'vueify-extract-css',
+ *   options: {
+ *       out: 'path/to/extracted/bundle.css'
+ *   }
+ * });
+ **/
+
+/**
  |--------------------------------------------------------------------------
  | Elixir Asset Management
  |--------------------------------------------------------------------------
@@ -33,44 +51,59 @@ elixir.config.js.browserify.transformers.push({
  |
  */
 elixir(function(mix) {
-    mix //.clean()
+    mix .clean()
         .less('application.less', 'public/css/application.css')
         .browserify('app.js', 'public/js/app.min.js')
-        .copy('resources/assets/js/jquery/jquery.min.js', 'public/js/jquery.min.js');
-    
-    mix.styles([
-        'material-design-icons/css/material-design-iconic-font.min.css',
-        'perfect-scrollbar/css/perfect-scrollbar.min.css'
-        ], 'public/css/vendors.min.css', 'resources/assets/less/plugins'
-    ).scripts([
+        .copy('resources/assets/js/jquery/jquery.min.js', 'public/js/jquery.min.js')
+        .styles([
+            'material-design-icons/css/material-design-iconic-font.min.css',
+            'perfect-scrollbar/css/perfect-scrollbar.min.css'],
+            'public/css/vendors.min.css', 'resources/assets/less/plugins'
+        ).scripts([
             'less/plugins/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js',
             'js/plugins/jquery.touchSwipe.min.js'
-        ],'public/js/plugins-vendor.min.js', './resources/assets');
-    
-    mix.uglify(['**/*.js', '!**/*.min.js', '!**/*.map'], 'public/js', {
-        mangle: true,
-        suffix: '.min.js'
-    });
-    
-    mix.version([
-        'css/application.css',
-        'css/vendors.min.css',
-        'js/app.min.js']);
-    
-    mix.copy('resources/assets/images', 'public/images')
-        .copy('resources/assets/fonts', 'public/build/fonts');
+            ],
+            'public/js/plugins-vendor.min.js', './resources/assets'
+        )
+        .scripts([
+                'js/arrow.js',
+                'js/password.js',
+                'js/inputField.js'],
+            'public/js/medlib.plugins.min.js', './resources/assets'
+        )
+        .uglify(['**/*.js', '!**/*.min.js', '!**/*.map'], 'public/js', {
+            mangle: true,
+            suffix: '.min.js'
+        })
+        .version([
+            'css/application.css',
+            'css/vendors.min.css',
+            'js/jquery.min.js',
+            'js/app.min.js'])
+        .copy('resources/assets/images', 'public/images')
+        .copy('resources/assets/fonts', 'public/build/fonts')
+        .clean([
+            'public/css',
+            'public/js'
+        ]);
 
     if (process.env.NODE_ENV !== 'production') {
 
-        mix.browserSync({
-            //proxy: '127.0.0.1:8000',
+        mix.artisanServe({
+            php_path: '/usr/local/bin/php',
+            artisan_path: './artisan',
+            host: '127.0.0.1',
+            port: 8000,
+            show_requests: true
+        }).browserSync({
+            proxy: '127.0.0.1:8000',
             files: [
                 elixir.config.appPath + '/**/*.php',
                 elixir.config.get('public.css.outputFolder') + '/**\/*.css',
                 elixir.config.get('public.versioning.buildFolder') + '/rev-manifest.json',
                 'resources/views/**/*.php'
             ],
-            browser: ['chrome']
+            browser: ['google chrome']
         });
     }
 });
